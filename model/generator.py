d4c32743596b014537481ac87054b3572bade149
Gustavo B.A. Gorgônio <ggorgon03@gmail.com>
2023-11-20 18:12:57 -0300

    chanded generator to restore its original functionality

--- model/generator.py
+++ model/generator.py
@@ -82,10 +82,10 @@ class Generator(nn.Module):
         self.hop_length = hp.audio.hop_length
         channel_size = hp.gen.channel_size
         kpnet_conv_size = hp.gen.kpnet_conv_size
-        self.latents_dim = hp.audio.latents_dim
+        # self.latents_dim = hp.audio.latents_dim
 
         # hop length between mel spectrograms and audio
-        self.mel_ar_token_ratio = hp.audio.latents_hop_length // hp.audio.hop_length
+        # self.mel_ar_token_ratio = hp.audio.latents_hop_length // hp.audio.hop_length
 
         self.res_stack = nn.ModuleList()
         hop_length = 1
@@ -112,7 +112,7 @@ class Generator(nn.Module):
             nn.Tanh(),
         )
 
-        self.upsampler = Upsampler(hp)
+        # self.upsampler = Upsampler(hp)
 
     def forward(self, c, z):
         '''
@@ -120,7 +120,7 @@ class Generator(nn.Module):
             c (Tensor): the conditioning sequence of mel-spectrogram (batch, mel_channels, in_length) 
             z (Tensor): the noise sequence (batch, noise_dim, in_length)
         '''
-        c_emb = self.upsampler(c)
+        # c_emb = self.upsampler(c)
 
         z = self.conv_pre(z)                # (B, c_g, L)
 
@@ -154,13 +154,13 @@ class Generator(nn.Module):
         # pad input mel with zeros to cut artifact
         # see https://github.com/seungwonpark/melgan/issues/8
 
-        #zero = torch.full((1, self.mel_channel, 10), -11.5129).to(c.device)
-        zero = torch.full((1, 10), 8193).to(c.device)
+        zero = torch.full((1, self.mel_channel, 10), -11.5129).to(c.device)
+        # zero = torch.full((1, 10), 8193).to(c.device)
         mel = torch.cat((c, zero), dim=-1).unsqueeze(0)
         print(mel.shape)
         if z is None:
-#            z = torch.randn(1, self.noise_dim, mel.size(2)).to(mel.device)
-            z = torch.randn(1, self.noise_dim, mel.size(2)*self.mel_ar_token_ratio).to(mel.device)
+            z = torch.randn(1, self.noise_dim, mel.size(2)).to(mel.device)
+            # z = torch.randn(1, self.noise_dim, mel.size(2)*self.mel_ar_token_ratio).to(mel.device)
 
         audio = self.forward(mel, z)
         audio = audio.squeeze() # collapse all dimension except time axis
